4e54b8deb4eda76e4a4e42c34798a4851bf9896f
Don-tresor05 <dontresor922@gmail.com>
2026-08-10 10:54:41 +0200

    feat(studio): register all new schema types

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,5 +1,29 @@
 import { localizedString } from "./localizedString";
 import { localizedText } from "./localizedText";
 import { newsPost } from "./newsPost";
+import { siteSettings } from "./siteSettings";
+import { memberChurch } from "./memberChurch";
+import { department } from "./department";
+import { galleryEvent } from "./galleryEvent";
+import { radioProgram } from "./radioProgram";
+import { testimonial } from "./testimonial";
+import { project } from "./project";
+import { departmentsPage } from "./departmentsPage";
+import { aboutPage } from "./aboutPage";
+import { secretariatPage } from "./secretariatPage";
 
-export const schemaTypes = [newsPost, localizedString, localizedText];
+export const schemaTypes = [
+  newsPost,
+  siteSettings,
+  memberChurch,
+  department,
+  galleryEvent,
+  radioProgram,
+  testimonial,
+  project,
+  departmentsPage,
+  aboutPage,
+  secretariatPage,
+  localizedString,
+  localizedText,
+];
